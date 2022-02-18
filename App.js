c1597f807d139e7dc328e86ce345670a50f5d098
Eric <eric.machamer@marquette.edu>
2022-02-18 14:50:53 -0600

    homerouting

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomePage from "./CustomerApp/HomePage";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  // return (
+  //   <View style={styles.container}>
+  //     <Text>Open up App.js to start working on your app!</Text>
+  //     <StatusBar style="auto" />
+  //   </View>
+  // );
+  return <HomePage />;
 }
 
 const styles = StyleSheet.create({
---
+++ CustomerApp/AcctSettings.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function AcctSettings() {
+  return (
+    <View>
+      <Text>AcctSettings</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
