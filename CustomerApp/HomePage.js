4e19106f71de76e582ae2f69bb4824d96fb00359
Jose Jaime Gutierrez <josejaimegutierrez@Joses-MacBook-Pro-2.local>
2022-02-23 14:21:52 -0600

    homepage finished

--- CustomerApp/HomePage.js
+++ CustomerApp/HomePage.js
@@ -1,7 +1,9 @@
-import { ImageBackground, Image, Button, StyleSheet, Text, View } from 'react-native'
+import { ImageBackground, Image, StyleSheet, Button, Text, View } from 'react-native'
 import React from 'react'
 
+
 const image = {uri: "https://play-lh.googleusercontent.com/ydkcSXReYXTWms08p6NbzkK85tOpQzcsM227Y2fVvyZXy5-TmZf594XfHZzATPjiFQ"}
+const map = {uri: "https://entrecourier.com/wp-content/uploads/2020/06/storemap.jpg.webp"}
 export default function HomePage() {
   return (
     <View style={styles.container}>
@@ -11,11 +13,28 @@ export default function HomePage() {
         source={image}
       />
       <View style={styles.rect1}/>
+      <Image
+        style={styles.map}
+        source={map}
+      />
+      <Text style={styles.text4}>Number of drivers in the area: 4</Text>
       <View style={styles.rect2}/>
-      <View style={styles.fixToText}>
-        <View style={styles.buttonsContainer}>
-        
-        </View>
+      <Text style={styles.text1}>Gas Price (per Gallon)</Text>
+      <Text style={styles.text2}>Regular:</Text>
+      <View style={styles.price1}><Text>$3.16</Text></View>
+      <Text style={styles.text3}>Premium:</Text>
+      <View style={styles.price2}><Text>$3.87</Text></View>
+      <View style={styles.button1}>
+        <Button
+          title="Terms and Conditions" color="white"
+          onPress={() => Alert.alert('url')}
+        />
+      </View>
+      <View style={styles.button2}>
+        <Button
+          title="Contact Us" color="white"
+          onPress={() => Alert.alert('Left button pressed')}
+        />
       </View>
       </ImageBackground>
     </View>
@@ -47,8 +66,7 @@ const styles = StyleSheet.create({
     borderWidth: 3,
     borderColor: '#000000',
     borderRadius: 10,
-  },
-  rect2: {
+  },rect2: {
     position: 'absolute',
     width: 350,
     height: 122,
@@ -59,8 +77,73 @@ const styles = StyleSheet.create({
     borderColor: '#000000',
     borderRadius: 10,
   },
-<<<<<<< HEAD
+  button1: {
+    position: 'absolute',
+    width: 195,
+    height: 40,
+    left: 30,
+    top: 690,
+    backgroundColor: '#B96835',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },button2: {
+    position: 'absolute',
+    width: 110,
+    height: 40,
+    left: 250,
+    top: 690,
+    backgroundColor: '#B96835',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  text1: {
+    position: 'absolute',
+    fontWeight: '500',
+    fontSize: 18,
+    lineHeight: 21,
+    top: 555,
+    alignSelf: 'center',
+  },text2: {
+    position: 'absolute',
+    fontSize: 18,
+    lineHeight: 21,
+    top: 585,
+    left: 105,
+  },text3: {
+    position: 'absolute',
+    fontSize: 18,
+    lineHeight: 21,
+    top: 620,
+    left: 105,
+  },
+  map: {
+    position: 'absolute',
+    width: 313,
+    height: 151,
+    left: 40,
+    top: 315,
+  },
+  text4: {
+    position: 'absolute',
+    fontWeight: 'bold',
+    fontSize: 18,
+    lineHeight: 21,
+    top: 489,
+    alignSelf: 'center',
+  },
+  price1: {
+    position: 'absolute',
+    width: 74,
+    height: 30,
+    left: 188,
+    top: 579,
+    backgroundColor: '#B96835',
+  },price2: {
+    position: 'absolute',
+    width: 74,
+    height: 30,
+    left: 188,
+    top: 616,
+    backgroundColor: '#B96835',
+  }
 })
-=======
-})
->>>>>>> 805f230c81375eec0c12e0bb576ad593713e2de5
