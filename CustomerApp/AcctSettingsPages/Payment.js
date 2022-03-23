69eaaafc3deacb43cd9a5c44a667ae21786ccec2
RyanR1019 <Rramirez1019@gmail.com>
2022-03-22 23:39:43 -0500

    BoundingBox

--- CustomerApp/AcctSettingsPages/Payment.js
+++ CustomerApp/AcctSettingsPages/Payment.js
@@ -11,27 +11,26 @@ import {
   UselessTextInput,
   Image,
 } from "react-native";
-import React, {Component} from "react";
-import firebase from 'firebase';
+import React, { Component } from "react";
+import firebase from "firebase";
 import { useState } from "react";
 import DropDownPicker from "react-native-dropdown-picker";
-import {getCards} from '../../firebasefunctions'
+import { getCards } from "../../firebasefunctions";
 import { ListItem } from "react-native-elements";
-import { auth } from '../../firebase'
+import { auth } from "../../firebase";
 // import TimeDropdown from "../dropdowns/TimeDropdown";
 // import DayDropdown from "../dropdowns/DayDropdown";
 // import GasDropdown from "../dropdowns/GasDropdown";
 // import PaymentDropdown from '../dropdowns/PaymentDropdown';
 
 // var useremail=auth.currentUser?.email;
-export default class Payment extends Component{
-
+export default class Payment extends Component {
   constructor() {
     super();
-    this.docs = firebase.firestore().collection('Credit_Cards');
+    this.docs = firebase.firestore().collection("Credit_Cards");
     this.state = {
       isLoading: true,
-      cards: []
+      cards: [],
     };
   }
 
@@ -39,7 +38,7 @@ export default class Payment extends Component{
     this.unsubscribe = this.docs.onSnapshot(this.getCardData);
   }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     this.unsubscribe();
   }
 
@@ -47,134 +46,140 @@ export default class Payment extends Component{
     const cards = [];
     querySnapshot.forEach((res) => {
       const { createdAt, cvv, email, expiry, number, type } = res.data();
-      if(email == auth.currentUser?.email){
-      cards.push({
-        key: res.id,
-        createdAt,
-        cvv,
-        email,
-        expiry,
-        number,
-        type
-      });
-    }
+      if (email == auth.currentUser?.email) {
+        cards.push({
+          key: res.id,
+          createdAt,
+          cvv,
+          email,
+          expiry,
+          number,
+          type,
+        });
+      }
     });
     this.setState({
       cards,
-      isLoading: false
-   });
-  }
-render(){
-  if(this.state.isLoading){
-    return(
-      <View style={styles.loader}>
-        <ActivityIndicator size="large" color="red"/>
-      </View>
-    )
-  }
+      isLoading: false,
+    });
+  };
+  render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.loader}>
+          <ActivityIndicator size="large" color="red" />
+        </View>
+      );
+    }
 
-  var count =0;
-  return (
-    <View style={styles.container}>
-      <ImageBackground
-        source={require("../../images/pumpfivebackground.jpeg")}
-        style={styles.image}
-      >
-        <SafeAreaView style={styles.container}>
-          
-            
-          <View style={styles.container}>
-            <View style={styles.Memberships}>
-              <View
-                style={{
-                  top: -0,
-                  flex: 1,
-                  flexDirection: "row",
-                  justifyContent: "space-around",
-                }}
-              >
-                <Text style={styles.text}>Payment Management</Text>
-                <View style={buttonstyles.backbutton}>
-                  <Button
-                    title="Back"
-                    color="white"
-                    onPress={() => this.props.navigation.goBack()}
-                  />
+    var count = 0;
+    return (
+      <View style={styles.container}>
+        <ImageBackground
+          source={require("../../images/pumpfivebackground.jpeg")}
+          style={styles.image}
+        >
+          <SafeAreaView style={styles.container}>
+            <View style={styles.container}>
+              <View style={styles.Memberships}>
+                <View
+                  style={{
+                    top: -0,
+                    flex: 1,
+                    flexDirection: "row",
+                    justifyContent: "space-around",
+                  }}
+                >
+                  <Text style={styles.text}>Payment Management</Text>
+                  <View style={buttonstyles.backbutton}>
+                    <Button
+                      title="Back"
+                      color="white"
+                      onPress={() => this.props.navigation.goBack()}
+                    />
+                  </View>
                 </View>
-              </View>
-              {/* <View style={{ bottom: "45%", left: "5%" }}>
+                {/* <View style={{ bottom: "45%", left: "5%" }}>
                 <Text style={styles.boxfontsbody}>{auth.currentUser?.email}</Text>
                 <Text style={styles.boxfontsbody}>Member no. 773123456789</Text>
                 <Text style={styles.boxfontsbody}>414-***-****</Text>
               </View> */}
 
-              {/* <View style={{ bottom: "55%",}}>
+                {/* <View style={{ bottom: "55%",}}>
                 <Text style={styles.creditdebit}>Credit/Debit Card (Add up to 3)</Text>
               </View>  */}
-                {
-                  this.state.cards.map((res, i) => {
-                    var image;
-                    var text;
-                    var lastfour;
+                {this.state.cards.map((res, i) => {
+                  var image;
+                  var text;
+                  var lastfour;
 
-                    count+=1;
-                    if(res.type=="visa"){
-                      image = require("../../icons/visa.png")
-                      text="Visa "
-                    }
-                    else if(res.type=="master-card"){
-                      image = require("../../icons/mastercard.png")
-                      text="Mastercard "
-                    }
+                  count += 1;
+                  if (res.type == "visa") {
+                    image = require("../../icons/visa.png");
+                    text = "Visa ";
+                  } else if (res.type == "master-card") {
+                    image = require("../../icons/mastercard.png");
+                    text = "Mastercard ";
+                  }
 
-                    var cardarray = res.number.split(" ")
-                    lastfour = cardarray[3]
-                    
-                    return (
-                      
-                      <View 
-                        style={{ top: "-35%", left: 10,}}
+                  var cardarray = res.number.split(" ");
+                  lastfour = cardarray[3];
+
+                  return (
+                    //It will go here
+                    <View style={styles.BoundingBox}>
+                      <View
+                        style={{
+                          top: "12%",
+                          left: "2%",
+                          bottom: "25%",
+
+                          textAlign: "center",
+                        }}
                       >
                         <View>
-                          <Text style={styles.bofadeeznutsbold}>Card {count}</Text>
+                          <Text style={styles.bofadeeznutsbold}>
+                            Card {count}
+                          </Text>
                         </View>
-                        <View style={{justifyContent: "center", alignItems: "center"}}>
+                        <View
+                          style={{
+                            justifyContent: "center",
+                            alignItems: "center",
+                          }}
+                        >
                           <Image source={image} />
                         </View>
                         <View>
-                          <Text style={styles.bofadeeznuts}>{text}ending in {lastfour}</Text>
+                          <Text style={styles.bofadeeznuts}>
+                            {text}ending in {lastfour}
+                          </Text>
                         </View>
                         <View>
-                          <Text style={styles.bofadeeznuts}>Exp: {res.expiry}</Text>
+                          <Text style={styles.bofadeeznuts}>
+                            Exp: {res.expiry}
+                          </Text>
                         </View>
                       </View>
-                      
-                    );
-                  })
-                }
-              
+                    </View>
+                  );
+                })}
 
-              <View style={buttonstyles.paybutton}>
-                <Button
-                  title="+ Add Payment Method"
-                  color="black"
-                  onPress={() => this.props.navigation.navigate('AddCard')}
-                />
-                 <Button
-                  title="Get Cards"
-                  color="black"
-                  onPress={getCards}
-                />
+                <View style={buttonstyles.paybutton}>
+                  <Button
+                    title="+ Add Payment Method"
+                    color="black"
+                    onPress={() => this.props.navigation.navigate("AddCard")}
+                  />
+                  <Button title="Get Cards" color="black" onPress={getCards} />
+                </View>
               </View>
-
             </View>
-          </View>
-          
-        </SafeAreaView>
-      </ImageBackground>
-    </View>
-  );
-}
+          </SafeAreaView>
+        </ImageBackground>
+      </View>
+    );
+  }
 }
 
 // function GasScreen({ navigation }) {
@@ -241,6 +246,7 @@ const styles = StyleSheet.create({
     borderRadius: 10,
   },
 
+  //Payment Management Bold Header
   text: {
     color: "black",
     fontSize: 40,
@@ -262,7 +268,21 @@ const styles = StyleSheet.create({
     left: 10,
   },
 
+  //Bounding Box
+  BoundingBox: {
+    //If overlay occurs with cars, delete position: absolute,
+    width: "100%",
+    height: "30%",
+    bottom: "30%",
+    backgroundColor: "#CDCABF",
+    borderWidth: 2,
+    borderColor: "#000000",
+    backgroundColor: "#FFFFFF",
+    borderRadius: 10,
+  },
+
   //bofadeeznuts
+  //Visa Ending in ####, Exp:
   bofadeeznuts: {
     color: "black",
     fontSize: 25,
@@ -391,9 +411,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#DAAC3F",
   },
   loader: {
-    position: 'absolute',
-    alignItems: 'center',
-    justifyContent: 'center',    
+    position: "absolute",
+    alignItems: "center",
+    justifyContent: "center",
     left: 0,
     right: 0,
     top: 0,
@@ -402,11 +422,11 @@ const styles = StyleSheet.create({
   rectangle: {
     height: "50%",
     width: "40%",
-    backgroundColor: 'white',
-    position: 'absolute', 
+    backgroundColor: "white",
+    position: "absolute",
     // zIndex: 99,
-    top: '50%',
-    left: '40%',
+    top: "50%",
+    left: "40%",
     borderWidth: 1,
   },
 });
